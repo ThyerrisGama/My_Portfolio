58baede8982f42b6659cef1848d472c321d3fba6
thyerris <thyerris@gmail.com>
2023-03-10 00:54:14 -0300

    webpack and babal config initial

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Home = () =>{
+    return(
+        <header className="Header-container">
+            <section className="Home-container">
+            </section>
+        </header>
+    );
+}
